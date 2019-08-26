3824d622c489ba314a0fa355fc47779a8251b235
John Renner <john@jrenner.net>
2019-08-26 14:28:20 -0700

    De-rustc-icize it

--- lib.rs
+++ lib.rs
@@ -17,15 +17,11 @@
 // this crate, which relies on this attribute (rather than the value of `--crate-name` passed by
 // cargo) to detect this crate.
 
-#![crate_name = "test"]
-#![unstable(feature = "test", issue = "50297")]
 #![doc(html_root_url = "https://doc.rust-lang.org/nightly/", test(attr(deny(warnings))))]
 #![feature(asm)]
 #![cfg_attr(any(unix, target_os = "cloudabi"), feature(libc, rustc_private))]
-#![feature(nll)]
 #![feature(set_stdio)]
 #![feature(panic_unwind)]
-#![feature(staged_api)]
 #![feature(termination_trait_lib)]
 #![feature(test)]
 
